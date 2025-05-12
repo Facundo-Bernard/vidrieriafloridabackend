dca236aa6725694d90d778916c76e7c5b6ca2505
facundobernard <rexlego21@gmail.com>
2025-05-11 21:59:20 -0300

    Actualizado el backend para usar el nuevo link

--- src/main/java/com/example/uade/tpo/practica2back/config/WebConfig.java
+++ src/main/java/com/example/uade/tpo/practica2back/config/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("https://vidrieriaflorida-front-u98r.vercel.app","https://vidrieriaeditorfront.vercel.app") // tu frontend de Vercel
+                    .allowedOrigins("https://vidrieriaflorida-front-u98r.vercel.app","https://vidrieriaeditorfront.vercel.app","https://www.vidrieriaflorida.com") // tu frontend de Vercel
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*")
                     .allowCredentials(true);
